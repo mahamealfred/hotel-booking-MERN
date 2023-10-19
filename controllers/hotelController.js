d6ad6123a6e1509d514defb92e5565e7af0081f5
mahamealfred <mahamealfred@gmail.com>
2023-10-19 06:57:13 -0700

    get all hotels

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -47,19 +47,21 @@ export const getHotel=async(req,res,next)=>{
             next(err)
            }
 }
-//get Hotel
 export const getHotels = async (req, res, next) => {
-    const { min, max, ...others } = req.query;
-    try {
-      const hotels = await Hotel.find({
-        ...others,
-        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
-      }).limit(req.query.limit);
-      res.status(200).json(hotels);
-    } catch (err) {
-      next(err);
+  const { min, max,limit, ...others } = req.query;
+  try {
+    const hotels = await Hotel.find(
+    {
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
     }
-  };
+    ).limit(limit);
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
+
   export const countByCity = async (req, res, next) => {
     const cities = req.query.cities.split(",");
     try {
